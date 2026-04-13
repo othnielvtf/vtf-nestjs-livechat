004a4b865549d6ab9ac92e15f650f4d34f2edf94
Othniel naga <othniel.naga@virtualtechfrontier.com>
2026-04-13 14:35:06 +0800

    Update deps

--- src/auth/users.service.ts
+++ src/auth/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 // In a real application, this would be stored in a database
 const USERS = [
